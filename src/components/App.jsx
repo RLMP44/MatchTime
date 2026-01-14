3ff4ded8e07d4d3244304772eeb24f154f601f63
Rachael Momber <rlmomber@gmail.com>
2026-01-14 17:52:33 -0500

    add: footer, fixes for folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
-import './App.css';
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <h1>body</h1>
+      <Footer />
     </div>
   );
 }
 
-export default App
+export default App;
