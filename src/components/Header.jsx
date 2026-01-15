edfe903a79f8245b329ba6cb2f32c7a7d9c0bf56
Rachael Momber <rlmomber@gmail.com>
2026-01-15 15:58:55 -0500

    update: styles and logos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
-import logo from "../assets/logo.jpg";
+import logo from "../assets/matchTimeFull.jpg";
 
 function Header() {
   return (
     <header>
       <img src={logo} alt="Match Time logo" />
-      <h1>Match Time</h1>
     </header>
   );
 }
