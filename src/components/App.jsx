4a78e6c6a3cdebb5d33e44855387f1d906e8139c
Rachael Momber <rlmomber@gmail.com>
2026-01-19 12:39:12 -0500

    add: initial setup of record display

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Timer from "./Timer";
+import RecordDisplay from "./RecordDisplay";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="timer-tab">
-        <h1>Records display</h1>
+        <RecordDisplay />
         <Timer />
       </div>
       <Footer />
---
+++ src/components/RecordDisplay.jsx
@@ -0,0 +1,19 @@
+function RecordDisplay() {
+  // async function createRecord() {
+
+  // }
+
+  return (
+    <div className="records-display-container">
+      <div className="record-container">
+        <p><strong>Place</strong></p>
+        <p><strong>Bib #</strong></p>
+        <p><strong>Time</strong></p>
+        <p><strong>Name</strong></p>
+      </div>
+    </div>
+  );
+
+}
+
+export default RecordDisplay;
