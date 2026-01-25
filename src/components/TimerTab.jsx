a94033940341c80fa24768baaed97c77a7b430bb
Rachael Momber <rlmomber@gmail.com>
2026-01-25 17:24:19 -0500

    update: move timer and record display into timer tab component

---
+++ src/components/TimerTab.jsx
@@ -0,0 +1,27 @@
+import Timer from "./Timer";
+import RecordDisplay from "./RecordDisplay";
+
+function TimerTab(props) {
+  return (
+    <div className="timer-tab">
+      <div className="records-display-container">
+        <RecordDisplay />
+        {props.displayRecords.map(record =>
+          <RecordDisplay
+            key={record.id}
+            data={record}
+            editRecords={props.editRecords}
+            fetchRecord={props.fetchRecord}
+            deleteRecord={props.deleteRecord}
+          />)}
+      </div>
+      <Timer
+        updateUserRecord={props.updateUserRecord}
+        fetchRecord={props.fetchUserRecord}
+        displayRecords={props.displayRecords}
+        />
+    </div>
+  );
+}
+
+export default TimerTab;
