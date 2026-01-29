bf9d8b8639e664e9d3b4897954690feae16e3c2e
Rachael Momber <rlmomber@gmail.com>
2026-01-29 16:49:14 -0500

    add: basic popup submit capability

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -2,37 +2,30 @@ import { useState } from "react";
 
 function Popup(props) {
   // TODO: make Popup component completely reusable and DRY
-  const [time, setTime] = useState(props?.data?.time || "");
-  const [bibNum, setBibNum] = useState(props?.data?.bib || "");
+  const [formData, setFormData] = useState({});
 
   function titleize(string) {
     return String(string).charAt(0).toUpperCase() + String(string).slice(1);
 }
 
-  // ----------------- RECORD EDIT LOGIC START -----------------
+
+  // ----------------- RACER HANDLING LOGIC -----------------
   async function handleSubmit(event) {
+    console.log(formData)
     const button = event.target.id;
-    if (button === "update-button") {
-      const user = (bibNum !== props.data.bib) ? await props.fetchRecord(parseInt(bibNum)): props.data;
-      const newTime = (time !== props.data.time) ? time : props.data.time;
-      // {...data} clones the data
+
+    if (button === 'save-button') {
+      // use object spreading to updated only updated fields in racer record
       var updatedRecord = {
         ...props.data,
-        id: user.id,
-        bib: user.bib,
-        time: newTime,
-        name: user.name
+        ...formData,
       };
-
-      props.editRecords({oldRecord: props.data, newRecord: updatedRecord});
-    } else if (button === "delete-button") {
-      props.deleteRecord(props.data);
-    } else {
+      props.editRacer({ newData: updatedRecord, oldData: props.data });
+    } else if (button === 'cancel-button') {
       console.log('cancelled');
     }
     props.setIsDisplayed("none");
   }
-  // ----------------- RECORD EDIT LOGIC END -----------------
 
   return (
     <div className="dialog">
@@ -42,12 +35,25 @@ function Popup(props) {
         {props.fields.map(field => {
           let title = field === "fName" ? "First Name" : field === "lName" ? "Last Name" : titleize(field);
           return (
-            <p key={field}>{title}: <input className={`${field}-input`} onChange={event => setTime(event.target.value)}></input></p>
+            <p key={field}>
+              {title}: <input className={`${field}-input`}
+                onChange={event =>
+                  setFormData({...formData, [field]: event.target.value})
+                }>
+              </input>
+            </p>
           );
         })}
         <div className="popup-buttons-container">
           {props.buttons.map(button =>
-            <button key={button} className="btn" id={`${button}-button`} onClick={handleSubmit}>{titleize(button)}</button>
+            <button
+              key={button}
+              className="btn"
+              id={`${button}-button`}
+              onClick={handleSubmit}
+            >
+              {titleize(button)}
+            </button>
           )}
         </div>
       </div>
