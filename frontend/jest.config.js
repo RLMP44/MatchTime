574adc3cf2a820d20904428ce1714e3ca76843e3
Rachael Momber <rlmomber@gmail.com>
2026-03-28 23:33:37 -0400

    add: component test for Timeer

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -4,5 +4,9 @@ export default {
   transform: {
     "^.+\\.(js|jsx)$": "babel-jest"
   },
-  moduleFileExtensions: ["js", "jsx"]
+  moduleFileExtensions: ["js", "jsx"],
+  moduleNameMapper: {
+    "^react$": "<rootDir>/node_modules/react",
+    "^react-dom$": "<rootDir>/node_modules/react-dom"
+  }
 };
